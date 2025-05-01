cdfbc160b51b29acbfe963f54c81824b9c3fae9e
Sp00kySkelet0n <spooky@spooky.net>
2025-05-01 23:16:42 +0200

    Change script to use docker image & modify README

--- README.md
+++ README.md
@@ -3,4 +3,9 @@ Jetbrain's C# decompiler on docker
 
 # What's this project ?
 
-This project is a Dockerfile to run Jetbrain's Dotpeek tool on linux using Wine over Docker
+This project is a Docker project to run Jetbrain's Dotpeek tool on linux using Wine in Docker
+
+# How to use ? 
+
+Simply launch ./dotpeek.sh and the script will pull the remote image from the github registry and launch it immediately ! 
+(I advise using my already built image as building the image locally is ~15-20 minutes long to install .NET in wine)
--- dotpeek.sh
+++ dotpeek.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-#sudo docker build -t wine_dotpeek .
-
-sudo docker run -it \
-     --rm \
-     --device /dev/snd \
-     --device=/dev/dri \
-     -e DISPLAY=$DISPLAY \
-     -v /tmp/.X11-unix:/tmp/.X11-unix:ro \
-     -v $(pwd):/mnt \
+sudo docker run -it \                                                                                                                                                                    ─╯
+    --rm \
+    --device /dev/snd \
+    --device=/dev/dri \
+    -e DISPLAY=$DISPLAY \
+    -v /tmp/.X11-unix:/tmp/.X11-unix:ro \
+    -v $(pwd):/mnt \
     ghcr.io/sp00p64/dotpeek_docker:latest
